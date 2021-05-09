61eee0d1528e504e07730e8da73c27761ebeeaed
Gabriel Hilarion <gabrielhilarion@gmail.com>
2021-05-09 21:03:45 +0100

    Create function to swith celcius and fehrenheit temp, add background images

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,50 @@
 const Dom = (() => {
-  const modalTitle = document.getElementById('staticBackdropLabel');
-  const country = document.getElementById('country');
+  const modalTitleDiv = document.getElementById('staticBackdropLabel');
+  const countryDiv = document.getElementById('country');
   const iconDiv = document.getElementById('iconDiv');
   const weatherDiv = document.getElementById('weatherDiv');
   const tempDiv = document.getElementById('temp');
+  const humidityDiv = document.getElementById('humidityDiv');
 
-  const changeTemperatureUnit = (unit = null) => {
-    const currentUnit = `unit is ${unit}`;
-    console.log(`${unit === null ? 'No unit suplied' : currentUnit}`);
+  const changeTemperatureUnit = (tempUnit) => {
+    const tempUnits = document.getElementById('temp-units');
+    const imperial = document.getElementById('imperial');
+    const metric = document.getElementById('metric');
+
+    const convertToMetric = () => {
+      tempDiv.textContent = tempDiv.getAttribute('data-celcius');
+    };
+
+    const convertToImperial = () => {
+      tempDiv.textContent = tempDiv.getAttribute('data-fahrenheit');
+    };
+
+    const highlightUnit = (unit = null) => {
+      tempUnits.querySelectorAll('button').forEach((elem) => {
+        elem.classList.remove('btn-primary');
+      });
+
+      if (unit === 'imperial') { imperial.classList.add('btn-primary'); }
+      if (unit === 'metric') { metric.classList.add('btn-primary'); }
+    };
+
+    highlightUnit(tempUnit);
+
+    imperial.onclick = (e) => {
+      if (!e.target.classList.contains('btn-primary')) {
+        const temp = Number(tempDiv.getAttribute('data-temp'));
+        highlightUnit(e.target.id);
+        convertToImperial(temp);
+      }
+    };
+
+    metric.onclick = (e) => {
+      if (!e.target.classList.contains('btn-primary')) {
+        const temp = Number(tempDiv.getAttribute('data-temp'));
+        highlightUnit(e.target.id);
+        convertToMetric(temp);
+      }
+    };
   };
 
   const inputsAreValid = (element) => {
@@ -24,6 +61,7 @@ const Dom = (() => {
     projectIputs.forEach((input) => input.addEventListener('focus', (e) => {
       e.target.style.borderColor = '#ccc';
     }));
+
     if (inputErrors) { return false; } return true;
   };
 
@@ -33,14 +71,25 @@ const Dom = (() => {
   };
 
   const displayData = (response) => {
-    if (response.cod === '404') {
-      return;
+    if (response.cod === '404' || response.error) {
+      modalTitleDiv.innerHTML = '<h4>There was an error</h4>';
+      countryDiv.innerHTML = response.error
+        ? '<h1 class="bg-light text-danger text-center">City may not exist</h1>'
+        : `<h1 class="bg-light text-danger text-center">${response.message}</h1>`;
+      weatherDiv.textContent = '';
+      humidityDiv.textContent = '';
+      tempDiv.textContent = '';
+      iconDiv.innerHTML = '';
+    } else {
+      modalTitleDiv.textContent = `Weather in ${response.name}`;
+      countryDiv.innerHTML = `<span class="weather_info"> ${response.name}, ${response.sys.country}</span>`;
+      weatherDiv.innerHTML = `<span class="weather_info">Weather: ${response.weather[0].main}, ${response.weather[0].description}</span>`;
+      humidityDiv.innerHTML = `<span class="weather_info">Humidity: ${response.main.humidity}</span>`;
+      tempDiv.textContent = `${response.main.temp}`;
+      tempDiv.setAttribute('data-celcius', response.celcius);
+      tempDiv.setAttribute('data-fahrenheit', response.fahrenheit);
+      iconDiv.innerHTML = `<img src='http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png'>`;
     }
-    modalTitle.textContent = `Search for ${response.name}`;
-    country.textContent = response.sys.country;
-    weatherDiv.textContent = response.weather[0].description;
-    tempDiv.textContent = response.main.temp;
-    iconDiv.innerHTML = `<img src='http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png'>`;
   };
 
   return {
--- src/weather.js
+++ src/weather.js
@@ -1,17 +1,28 @@
 import Dom from './dom';
 
 const Weather = (() => {
-  const getWeather = async (location) => {
+  const getWeather = async (location, tempUnit) => {
     try {
       const key = 'ee8f07d3dc0822c9454ff3c4ba6c3f02';
-      const getDetails = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`,
+      const getDetails = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=${tempUnit}`,
         { mode: 'cors' });
+
       const jsonData = await getDetails.json();
+      Dom.changeTemperatureUnit(tempUnit);
+
+      if (tempUnit === 'metric') {
+        jsonData.celcius = Number(jsonData.main.temp).toFixed(0);
+        jsonData.fahrenheit = ((jsonData.main.temp * (9 / 5)) + 32).toFixed(0);
+      } else {
+        jsonData.fahrenheit = Number(jsonData.main.temp).toFixed(0);
+        jsonData.celcius = (jsonData.main.temp - 32) * (5 / 9).toFixed(0);
+      }
+
       return jsonData;
     } catch (error) {
-      console.log(`Error:  ${error}`);
+      Dom.changeTemperatureUnit(tempUnit);
+      return { error };
     }
-    return '';
   };
 
   const weatherData = () => {
@@ -20,7 +31,8 @@ const Weather = (() => {
       const element = document.getElementById('centered');
       if (Dom.inputsAreValid(element)) {
         const location = document.getElementById('city').value.trim();
-        getWeather(location).then((response) => {
+        const tempUnit = document.getElementById('temp-unit').value;
+        getWeather(location, tempUnit).then((response) => {
           Dom.displayData(response);
           Dom.openModal();
         });
